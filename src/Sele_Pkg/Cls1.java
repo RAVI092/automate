d16a257d3d85fdb4a8424f9feb37d49120470917
RAVI092 <ravi.shekhar092@outlook.com>
2019-11-22 13:56:28 +0530

    File commited

---
+++ src/Sele_Pkg/Cls1.java
@@ -0,0 +1,12 @@
+package Sele_Pkg;
+
+public class Cls1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Class1");
+
+	}
+
+}
---
+++ src/Sele_Pkg/cls2.java
@@ -0,0 +1,10 @@
+package Sele_Pkg;
+
+public class cls2 {
+
+	public static void main(String[] args) {
+		System.out.println("Class2");
+
+	}
+
+}
