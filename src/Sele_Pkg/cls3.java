ae975be018fff46ea7b112e9fd959a091bf5f56c
RAVI092 <ravi.shekhar092@outlook.com>
2019-11-22 14:11:02 +0530

    added new class3

---
+++ src/Sele_Pkg/cls3.java
@@ -0,0 +1,13 @@
+package Sele_Pkg;
+
+public class cls3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("Class3");
+		System.out.println("Class4");
+		
+	}
+
+}
